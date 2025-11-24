31485c1f279b79be1e9c110b6575f9a2c5076bdd
장경재 <developiaa@gmail.com>
2025-11-24 20:00:00 +0900

    [2025-12] 모듈 추가

---
+++ _2025_12/src/test/java/study/developia/_2025_12/ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.developia._2025_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
