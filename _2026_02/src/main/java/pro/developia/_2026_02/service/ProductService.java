76da82658ffc1082300104031e9e0e0523a91e65
장경재 <developiaa@gmail.com>
2026-02-08 14:00:00 +0900

    [2026-02] getOrDefault 메소드 검증

--- _2026_02/src/main/java/pro/developia/_2026_02/service/ProductService.java
+++ _2026_02/src/main/java/pro/developia/_2026_02/service/ProductService.java
@@ -1,17 +1,40 @@
 package pro.developia._2026_02.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pro.developia._2026_02.annotation.Sharding;
 import pro.developia._2026_02.domain.Product;
+import pro.developia._2026_02.domain.ProductStatus;
 import pro.developia._2026_02.repository.ProductRepository;
 import pro.developia._2026_02.service.dto.ProductDto;
 
+import java.time.LocalDate;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public ProductDto getOrDefault(String productId) {
+        Product product = productRepository.findById(productId)
+                .orElseGet(() -> Product.builder()
+                        .productId("new" + productId)
+                        .productName("test")
+                        .manufacturer("test")
+                        .salesPrice(10000)
+                        .stockQuantity(100)
+                        .brand("test")
+                        .salesStartDate(LocalDate.now())
+                        .salesEndDate(LocalDate.now().plusMonths(1))
+                        .productStatus(ProductStatus.AVAILABLE)
+                        .build());
+
+        return ProductDto.from(product);
+    }
+
 
     /**
      * hash sharding
@@ -34,7 +57,7 @@ public class ProductService {
         return product.getProductId();
     }
 
-    /**
+    /*
      * range sharding
      */
 
---
+++ _2026_02/src/test/java/pro/developia/_2026_02/service/ProductServiceTest.java
@@ -0,0 +1,77 @@
+package pro.developia._2026_02.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.developia._2026_02.domain.Product;
+import pro.developia._2026_02.domain.ProductStatus;
+import pro.developia._2026_02.repository.ProductRepository;
+import pro.developia._2026_02.service.dto.ProductDto;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @InjectMocks
+    ProductService productService;
+    @Mock
+    ProductRepository productRepository;
+
+    @DisplayName("상품이 있는 경우 조회한 값을 가져온다.")
+    @Test
+    void test1() {
+        String productId = "test";
+        LocalDate now = LocalDate.now();
+        Product product = Product.builder()
+                .productId(productId)
+                .productName("test")
+                .manufacturer("test")
+                .salesPrice(10000)
+                .stockQuantity(100)
+                .brand("test")
+                .salesStartDate(now)
+                .salesEndDate(now.plusMonths(1))
+                .productStatus(ProductStatus.AVAILABLE)
+                .build();
+
+        given(productRepository.findById(productId))
+                .willReturn(Optional.of(product));
+
+        ProductDto result = productService.getOrDefault(productId);
+
+        assertThat(result.getProductId()).isEqualTo(productId);
+    }
+
+    @DisplayName("상품이 없는 경우 기본 상품을 가져온다.")
+    @Test
+    void test2() {
+        String productId = "test";
+        LocalDate now = LocalDate.now();
+        Product product = Product.builder()
+                .productId(productId)
+                .productName("test")
+                .manufacturer("test")
+                .salesPrice(10000)
+                .stockQuantity(100)
+                .brand("test")
+                .salesStartDate(now)
+                .salesEndDate(now.plusMonths(1))
+                .productStatus(ProductStatus.AVAILABLE)
+                .build();
+
+        given(productRepository.findById(productId))
+                .willReturn(Optional.empty());
+
+        ProductDto result = productService.getOrDefault(productId);
+
+        assertThat(result.getProductId()).isEqualTo("new" + productId);
+    }
+
+}
