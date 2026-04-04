bbe1598c043b9e02843adbab03a54d6b60404d7b
장경재 <developiaa@gmail.com>
2026-04-05 07:00:00 +0900

    [2026-04-02] 테스트 분리

--- _2026_04_02/src/test/kotlin/pro/developia/_2026_04_02/repository/ExposedCardIssueRepositoryTest.kt
+++ _2026_04_02/src/test/kotlin/pro/developia/_2026_04_02/repository/ExposedCardIssueRepositoryTest.kt
@@ -3,6 +3,7 @@ package pro.developia._2026_04_02.repository
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
@@ -14,34 +15,45 @@ import kotlin.test.Test
 class ExposedCardIssueRepositoryTest {
     @Autowired
     private lateinit var repository: ExposedCardIssueRepository
+    val userId = 1004L
+    var id: Long? = null
+
+    @BeforeEach
+    fun setUp() {
+        id = repository.createIssue(userId)
+    }
 
     @Test
-    fun `Exposed CRUD가 정상적으로 동작한다`() {
-        // 1. CREATE 테스트
-        val userId = 1004L
+    fun create() {
         val issueId = repository.createIssue(userId)
 
         issueId.shouldNotBeNull()
+    }
 
-        // 2. READ 테스트
-        val savedIssue = repository.findById(issueId)
+    @Test
+    fun read() {
+        val savedIssue = repository.findById(id!!)
 
         savedIssue.shouldNotBeNull()
         savedIssue.userId shouldBe userId
         savedIssue.status shouldBe IssueStatus.PENDING
+    }
 
-        // 3. UPDATE 테스트
-        val isUpdated = repository.updateStatus(issueId, IssueStatus.SUCCESS)
+    @Test
+    fun update() {
+        val isUpdated = repository.updateStatus(id!!, IssueStatus.SUCCESS)
         isUpdated shouldBe true
 
-        val updatedIssue = repository.findById(issueId)
+        val updatedIssue = repository.findById(id!!)
         updatedIssue?.status shouldBe IssueStatus.SUCCESS
+    }
 
-        // 4. DELETE 테스트
-        val isDeleted = repository.deleteById(issueId)
+    @Test
+    fun delete() {
+        val isDeleted = repository.deleteById(id!!)
         isDeleted shouldBe true
 
-        val deletedIssue = repository.findById(issueId)
+        val deletedIssue = repository.findById(id!!)
         deletedIssue.shouldBeNull()
     }
 
